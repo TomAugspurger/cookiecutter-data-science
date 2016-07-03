2d662a8c102f48d041abd2dce2d0f87a93414c31
Tom Augspurger <tom.augspurger88@gmail.com>
2016-07-03 13:28:38 -0500

    setup.py and versioneer

---
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from os import path
+import versioneer
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name="{{ cookiecutter.project_name }}",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+
+    description='',
+    long_description='',
+
+    # Author details
+    author='Tom Augspurger',
+    author_email='tom.w.augspurger@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='data analysis',
+    packages=find_packages(exclude=['tests']),
+    extras_require={
+        'dev': ['cython', 'numpydoc'],
+        'test': ['coverage', 'pytest'],
+    },
+
+)
